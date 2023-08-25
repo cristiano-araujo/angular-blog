f2826d9c4086d8168cd52df6a641e52fa8263891
Cristiano Araújo <cristiano112715@gmail.com>
2023-08-25 19:41:56 -0300

    123

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,15 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "Renata Souza agora também é Moda Intima",
-    "description": "Estamos trabalhando com Moda Intima no renatasouza_modaintima",
-    "photoCover":"https://yt3.googleusercontent.com/0HGJlxzumZUSESSu2Kj0PjlpxNmnhal-NxyE4tp7AoZmnhbaQtw4I1w-LLCiz28xF8SyydiQpA=s176-c-k-c0x00ffffff-no-rj"
+    "title": "App Rede Saúde!",
+    "description": "Vantagens que você só encontra na Rede Saúde. Pronto para usar: Após a assinatura, você tem acesso imediato a nossa rede e já pode marcar as suas consultas ou exames, sem qualquer período de carência. A Rede que Não Para: Estamos crescendo toda semana, ampliando ainda mais suas opções.  Você com acesso: Tenha acesso aos principais médicos e laboratórios que atendem na maioria dos planos de saúde. Valores acessíveis: Realize consultas e exames sem pesar no bolso. Você se conectando: Pelo nosso app, entre em contato diretamente com as clínicas via WhatsApp ou telefone. Você no Controle: Na Rede Saúde, idade não é barreira. Todos têm acesso, independentemente da idade. Muito fácil incluir: Assine e inclua até 3 dependentes sem restrições de vínculo. Facilidade no Pagamento: Pague sua consulta via cartão de crédito, PIX ou boleto. https://www.appredesaude.com.br/_pflandpage/ ",
+    "photoCover":"https://appredesaude.com.br/img/logo.png "
   },
   {
     "id":"2",
     "title": "Realizamos um mesversario diferente!",
-    "description": "",
+    "description": 
+    "",
     "photoCover":"https://yt3.googleusercontent.com/0HGJlxzumZUSESSu2Kj0PjlpxNmnhal-NxyE4tp7AoZmnhbaQtw4I1w-LLCiz28xF8SyydiQpA=s176-c-k-c0x00ffffff-no-rj"
   },
   {
